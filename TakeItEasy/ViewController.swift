f351a32752d2cbad248b54606958ee5141528d3b
Corey <coreydash7@gmail.com>
2022-06-07 16:21:34 -0400

    Initial Commit

---
+++ TakeItEasy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TakeItEasy
+//
+//  Created by Corey Augburn on 6/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
